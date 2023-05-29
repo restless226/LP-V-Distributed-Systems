a6f7e966242a174714bf936e53fdd2a39a7b7b53
restless226 <rapendse2002@gmail.com>
2023-05-29 18:51:56 +0530

    Token Ring

--- src/Assignment5/TokenRing.java
+++ src/Assignment5/TokenRing.java
@@ -3,59 +3,39 @@ package Assignment5;
 import java.util.*;
 
 public class TokenRing {
-    public static void viewRing(int n) {
+    public void viewRing(int n) {
         System.out.println("Ring formed is as below:");
-        for (int i = 0; i < n - 1; i++) {
-            System.out.print(i + "->");
-        }
-        System.out.println("0");
+        for (int i = 0; i < n - 1; i++) System.out.print(i + "->");
+        System.out.println("0\n");
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter no of nodes in the ring: ");
         int n = scanner.nextInt();
-
-        TokenRing.viewRing(n);
-
-        int token = 0;
+        TokenRing tokenRing = new TokenRing();
+        tokenRing.viewRing(n);
         int choice = 1;
-
         while (choice == 1) {
-            int sender, receiver;
-            String message;
-
+            int token = 0;
             System.out.println("Enter sender: ");
-            sender = scanner.nextInt();
-
+            int sender = scanner.nextInt();
             System.out.println("Enter receiver: ");
-            receiver = scanner.nextInt();
-
+            int receiver = scanner.nextInt();
             scanner.nextLine();
             System.out.println("Enter message: ");
-            message = scanner.nextLine();
-
+            String message = scanner.nextLine();
             System.out.println("Passing token from node 0 to sender node...");
             for (int i = token; i < sender; i++) {
                 System.out.println("At node " + i);
             }
-            System.out.println();
-
-            System.out.println("At sender node: " + sender + " Sending The message: " + message);
-            System.out.println();
-
-            if (receiver == sender + 1) {
-                System.out.println("Receiver: " + receiver + " Received The message: " + message);
-            } else {
+            System.out.println("\nAt sender node: " + sender + " Sending The message: " + message + "\n");
+            if (receiver != sender + 1) {
                 for (int i = sender; i != receiver; i = (i + 1) % n) {
                     System.out.println("message: " + message + " Forwarded By: " + i);
                 }
-                System.out.println("Receiver: " + receiver + " Received The message: " + message);
             }
-            System.out.println();
-
-            token = sender;
-
+            System.out.println("\nReceiver: " + receiver + " Received The message: " + message + "\n");
             System.out.print("Do You Want To Send message Again? If YES Enter 1, If NO Enter 0: ");
             choice = scanner.nextInt();
         }
